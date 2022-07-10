49fe9c826e4d1b5d64399ebe709f5a35d362926d
sid_stha7 <sidstha444@gmail.com>
2022-07-10 20:54:43 +0545

    mern stack authentication

---
+++ client/src/components/main/index.js
@@ -0,0 +1,23 @@
+import styles from "./styles.module.css";
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+
+	return (
+		<div className={styles.main_container}>
+			<nav className={styles.navbar}>
+				<h1>Dashboard</h1>
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Logout
+				</button>
+				
+			</nav>
+		<h1>Welcome To Dashboard</h1>
+		</div>
+	);
+};
+
+export default Main;
